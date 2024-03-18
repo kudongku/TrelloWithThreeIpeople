9efe10cbc8605681468cfdbec6ec7399c5bacaa9
kiwimel0n <alstkd8580@naver.com>
2024-03-18 12:20:26 +0900

    init commit

---
+++ src/test/java/com/sparta/trellowiththreeipeople/TrelloWithThreeIpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.trellowiththreeipeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloWithThreeIpeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
